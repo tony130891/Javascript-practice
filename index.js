69cc8c0e4628f1935c7c9b73d9ea25ecc0a9c139
Tony <antonivelasquez67@gmail.com>
2023-04-30 12:17:26 -0400

    first attempt

--- index.js
+++ index.js
@@ -1,15 +1,59 @@
 function getComputerChoice() {
-  const choices = ["Rock", "Paper", "Scissors"];
-  const randomChoices = choices[Math.floor(Math.random() * choices.length)];
+  const option = ["Rock", "Paper", "Scissors"];
+  const randomChoices = option[Math.floor(Math.random() * option.length)];
   return randomChoices;
 }
 
-function rounds(playerSelection, computerSelection) {
-  if ("rock" == "Paper") {
-    return "you lose! Paper beats rock";
-  } else if ("rock" == "scissors") {
-    return "you won!";
+function winner(playerSelection, computerSelection) {
+  if (playerSelection == computerSelection) {
+    return "draw";
+  } else if (
+    (playerSelection == "rock" && computerSelection == "scissors") ||
+    (playerSelection == "scissors" && computerSelection == "paper") ||
+    (playerSelection == "paper" && computerSelection == "rock")
+  ) {
+    return "player";
   } else {
-    return "give it another try!";
+    return "computer";
   }
 }
+
+function rounds(playerSelection, computerSelection) {
+  const result = winner(playerSelection, computerSelection);
+  if (result == "draw") {
+    return "it's a draw";
+  } else if (result == "player") {
+    return "You won";
+  } else {
+    return "The pc won";
+  }
+}
+
+function getPlayerChoice() {
+  let validateInput = false;
+  while (validateInput == false) {
+    const choice = prompt("rock paper scissors");
+    if (choice == null) {
+      continue;
+    }
+    const choiceLower = choice.toLowerCase();
+    If(option.includes(choiceLower));
+    {
+      validateInput = true;
+      return choiceLower;
+    }
+  }
+}
+
+getPlayerChoice();
+
+function game() {
+  console.log("welcome");
+  for (let i = 0; i < 5; i++) {
+    const playerSelection = getPlayerChoice();
+    const computerSelection = getComputerChoice();
+    console.log(rounds(playerSelection, computerSelection));
+  }
+}
+
+game();
