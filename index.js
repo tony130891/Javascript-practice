7a3a5f563d871e058c04317af3973ade63575d55
Tony <antonivelasquez67@gmail.com>
2023-05-15 12:56:51 -0400

    almost there

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+let playerScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
   const choices = ["rock", "paper", "scissors"];
   const randomChoices = Math.floor(Math.random() * 3);
@@ -5,22 +8,40 @@ function getComputerChoice() {
   return computerChoice;
 }
 
-getComputerChoice();
-
 const playRound = (playerSelection, computerSelection) => {
   if (playerSelection === computerSelection) {
     return "it's a tie";
   } else if (playerSelection === "rock" && computerSelection === "scissors") {
+    playerScore++;
     return "you win";
   } else if (playerSelection === "paper" && computerSelection === "rock") {
+    playerScore++;
     return "you win";
   } else if (playerSelection === "scissors" && computerSelection === "paper") {
+    playerScore++;
     return "you win";
   } else {
+    computerScore++;
     return "you lose";
   }
 };
 
-const playerSelection = "rock";
 const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+
+const game = () => {
+  for (let i = 0; i < 5; i++) {
+    const playerSelection = prompt("choose the option");
+    const computerSelection = getComputerChoice();
+    playRound(playerSelection, computerSelection);
+  }
+
+  if (playerScore > computerScore) {
+    return "you have beaten the pc";
+  } else if (playerScore < computerScore) {
+    return "you got beaten by the pc";
+  } else {
+    return "you tied the game";
+  }
+};
+
+console.log(game());
