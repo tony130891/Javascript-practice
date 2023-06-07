ce7bbd152130147b13797f8a595032d6b34243c6
Tony <antonivelasquez67@gmail.com>
2023-06-07 16:22:29 -0400

    js last file

--- rps.js
+++ rps.js
@@ -2,6 +2,8 @@ const paperBtn = document.querySelector(".paper");
 const rockBtn = document.querySelector(".rock");
 const scissorsBtn = document.querySelector(".scissors");
 const outcomeDiv = document.querySelector(".outcome");
+const playerScoreSpan = document.querySelector(".player-score");
+const computerScoreSpan = document.querySelector(".computer-score");
 let playerScore = 0;
 let computerScore = 0;
 
@@ -13,70 +15,64 @@ function getComputerChoice() {
 }
 
 const playRound = (playerSelection, computerSelection) => {
-  console.log("1 ", playerSelection, "2 ", computerSelection);
+  const para = document.createElement("p");
   if (playerSelection === computerSelection) {
-    const para = document.createElement("p");
     para.innerText = "it's a tie";
-    outcomeDiv.appendChild(para);
   } else if (playerSelection === "rock" && computerSelection === "scissors") {
     playerScore++;
-    const para = document.createElement("p");
     para.innerText = "you win";
-    outcomeDiv.appendChild(para);
   } else if (playerSelection === "paper" && computerSelection === "rock") {
     playerScore++;
-    const para = document.createElement("p");
     para.innerText = "you win";
-    outcomeDiv.appendChild(para);
   } else if (playerSelection === "scissors" && computerSelection === "paper") {
     playerScore++;
-    const para = document.createElement("p");
     para.innerText = "you win";
-    outcomeDiv.appendChild(para);
   } else {
     computerScore++;
-    const para = document.createElement("p");
     para.innerText = "you lose";
-    outcomeDiv.appendChild(para);
   }
+  outcomeDiv.appendChild(para);
+};
+
+const checkForWinner = (playerScore, computerScore) => {
+  if (playerScore === 5) {
+    const h2 = document.createElement("h2");
+    h2.innerText = `you won ${playerScore} times against ${computerScore}`;
+  }
+  if (computerScore === 5) {
+    const h2 = document.createElement("h2");
+    h2.innerText = `you lost ${computerScore} times against ${playerScore}`;
+  }
+  outcomeDiv.append(h2);
 };
 
 const computerSelection = getComputerChoice();
+
+const updatedScore = (playerScore, computerScore) => {
+  playerScoreSpan.textContent = `player score: ${playerScore}     `;
+  computerScoreSpan.textContent = `computer score: ${computerScore}   `;
+};
 
 rockBtn.addEventListener("click", () => {
   const computerSelection = getComputerChoice();
   const playerSelection = "rock";
   playRound(playerSelection, computerSelection);
+  updatedScore(playerScore, computerScore);
+  checkForWinner(playerScore, computerScore);
 });
 
 paperBtn.addEventListener("click", () => {
   const computerSelection = getComputerChoice();
   const playerSelection = "paper";
   playRound(playerSelection, computerSelection);
+  updatedScore(playerScore, computerScore);
+  checkForWinner(playerScore, computerScore);
 });
 
 scissorsBtn.addEventListener("click", () => {
   const computerSelection = getComputerChoice();
   const playerSelection = "scissors";
   playRound(playerSelection, computerSelection);
+  updatedScore(playerScore, computerScore);
+  checkForWinner(playerScore, computerScore);
 });
-
-const game = () => {
-  // for (let i = 0; i < 5; i++) {
-  //   const playerSelection = prompt(
-  //    "choose the option, paper, rock or scissors"
-  //   ).toLowerCase;
-  //   const computerSelection = getComputerChoice();
-  //   playRound(playerSelection, computerSelection);
-  // }
-
-  if (playerScore > computerScore) {
-    return "you have beaten the pc";
-  } else if (playerScore < computerScore) {
-    return "you got beaten by the pc";
-  } else {
-    return "you tied the game";
-  }
-};
-
-console.log(game());
